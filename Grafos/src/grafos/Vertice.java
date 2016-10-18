03a9108b36ee20fdc6105312ffe426089c9f9760
Luiz Fernando <>
2016-10-18 11:31:02 -0200

    Vertice e grafo

---
+++ Grafos/src/grafos/Vertice.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grafos;
+
+/**
+ *
+ * @author Anah
+ */
+public class Vertice {
+    int nome;
+    
+    public Vertice(int nome) {
+        this.nome = nome;
+    }
+}
