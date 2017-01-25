24de1810286f3b9421267da74f87c3d45354fdb9
Luiz Fernando <>
2017-01-25 10:52:12 -0200

    Vertice terminado

---
+++ Grafos/src/model/Vertice.java
@@ -0,0 +1,28 @@
+package model;
+
+public class Vertice {
+    String id;
+    int grau;
+
+    public Vertice() {
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public int getGrau() {
+        return grau;
+    }
+
+    public void setGrau(int grau) {
+        this.grau = grau;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    
+}
