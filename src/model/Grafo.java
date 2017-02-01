239c040b61e30fbf473cdb0c3b0ac84d3f1f50ed
ramonftb <>
2017-02-01 09:54:41 -0200

    grafo/aresta terminado.

---
+++ src/model/Grafo.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author Alexandre
+ */
+public class Grafo {
+
+    private String id, edgedefault;
+    private List<Aresta> listaAresta = new ArrayList<Aresta>();
+
+    public Grafo() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEdgedefault() {
+        return edgedefault;
+    }
+
+    public void setEdgedefault(String edgedefault) {
+        this.edgedefault = edgedefault;
+    }
+
+    public List<Aresta> getListaAresta() {
+        return listaAresta;
+    }
+
+    public void setlistaAresta(List<Aresta> Aresta) {
+        this.listaAresta = listaAresta;
+    }
+
+    public void addAresta(Aresta aresta) {
+        this.listaAresta.add(aresta);
+    }
+
+}
